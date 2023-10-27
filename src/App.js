27335bcab7ea8498ddfddcf9198669e57435af4d
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-10-27 18:29:38 +0330

    HOC & withQuestion

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import "./App.css"
 
-import Counter from "./practice/counter"
-import Temp from "./practice/temp"
-import NotifCount from "./practice/notifCount"
-import ListUser from "./practice/users/ListUser"
-import Inputs from "./practice/inputs"
-import Form from "./practice/Form/Form"
-import Quiz from "./practice/Quiz/Quiz"
-import Ticket from "./practice/Ticket/Ticket"
-import MethodProp from "./practice/testMethod/method-prop"
+// import Counter from "./practice/counter"
+// import Temp from "./practice/temp"
+// import NotifCount from "./practice/notifCount"
+// import ListUser from "./practice/users/ListUser"
+// import Inputs from "./practice/inputs"
+// import Form from "./practice/Form/Form"
+// import Quiz from "./practice/Quiz/Quiz"
+// import Ticket from "./practice/Ticket/Ticket"
+// import MethodProp from "./practice/testMethod/method-prop"
 
-import Header from "./practice/BookList/Header"
-import AddForm from "./practice/BookList/AddForm"
+// import Header from "./practice/BookList/Header"
+// import AddForm from "./practice/BookList/AddForm"
 
-import Shop from "./practice/Shop/Shop"
-import TodoList from "./practice/TodoList/TodoList"
-import NoteApp from "./practice/NoteApp/NoteApp"
+// import Shop from "./practice/Shop/Shop"
+// import TodoList from "./practice/TodoList/TodoList"
+// import NoteApp from "./practice/NoteApp/NoteApp"
 
-import Alert from "react-bootstrap/Alert"
-import Accordion from "react-bootstrap/Accordion"
-import Button from 'react-bootstrap/Button';
-import BootstarpTest from "./practice/bootstarp-test"
+// import Alert from "react-bootstrap/Alert"
+// import Accordion from "react-bootstrap/Accordion"
+// import Button from 'react-bootstrap/Button';
+// import BootstarpTest from "./practice/bootstarp-test"
 
-import CourseResponsive from "./practice/course-responsive/course-responsive"
+// import CourseResponsive from "./practice/course-responsive/course-responsive"
 
-import FetchTest from "./practice/fetch-test"
+// import FetchTest from "./practice/fetch-test"
 
-import Router from "./practice/test-router/Router"
-import Router2 from "./practice/test-router2/Router"
+// import Router from "./practice/test-router/Router"
+// import Router2 from "./practice/test-router2/Router"
 
 // import { Route,Routes } from 'react-router-dom'
 // import Home from "./practice/test-router/Home"
@@ -36,20 +36,23 @@ import Router2 from "./practice/test-router2/Router"
 // import Products from "./practice/test-router/Products"
 
 
-import Users from "./usersFire/Users"
+// import Users from "./usersFire/Users"
 
-import Rechart from "./practice/rechart"
-import CustomHook from "./practice2/customHooks/customHook"
-import Counter2 from "./practice2/counter2/Counter2"
-import FetchPost from "./practice2/fetchPosts/fetchPost"
-import LocalStorage from "./practice2/localStorage/LocalStorage"
-import MemoHook from "./practice2/memoHook"
-import MemoHook2 from "./practice2/MemoHook2"
-import UseCallback from "./practice2/UseCallback"
-import UseRef from "./practice2/useRef/UseRef"
-import UseRef2 from "./practice2/useRef/UseRef2"
-import Timer from './practice2/useRef/Timer'
-import Todos from "./practice2/pagenation/Todos"
+// import Rechart from "./practice/rechart"
+// import CustomHook from "./practice2/customHooks/customHook"
+// import Counter2 from "./practice2/counter2/Counter2"
+// import FetchPost from "./practice2/fetchPosts/fetchPost"
+// import LocalStorage from "./practice2/localStorage/LocalStorage"
+// import MemoHook from "./practice2/memoHook"
+// import MemoHook2 from "./practice2/MemoHook2"
+// import UseCallback from "./practice2/UseCallback"
+// import UseRef from "./practice2/useRef/UseRef"
+// import UseRef2 from "./practice2/useRef/UseRef2"
+// import Timer from './practice2/useRef/Timer'
+// import Todos from "./practice2/pagenation/Todos"
+// import Main from "./practice3/hocsTest/main"
+// import WithToggle from "./practice3/hocsTest2/WithToggle"
+import WithAccordion from "./practice3/withAccordion/WithAccordion"
 export default function App() {
   return (
     <div className="App">
@@ -103,7 +106,10 @@ export default function App() {
       {/* <UseRef /> */}
       {/* <UseRef2 /> */}
       {/* <Timer /> */}
-      <Todos />
+      {/* <Todos /> */}
+      {/* <Main /> */}
+      {/* <WithToggle /> */}
+      <WithAccordion />
     </div>
   )
 }
